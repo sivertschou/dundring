41134fc64e84e1cab2ef7f3f6a8dea69fad89826
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-09-28 23:34:24 +0200

    Basic logging

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import * as React from "react";
 import { useHeartRate } from "./hooks/useHeartRate";
 import theme from "./theme";
 
+interface DataPoint {
+  heartRate?: number;
+  timeStamp: Date;
+}
+
 export const App = () => {
   const { requestHRPermission, heartRate, isConnected, disconnect } =
     useHeartRate();
+
+  const [data, setData] = React.useState([] as DataPoint[]);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (isConnected) {
+        console.log("heartRate:", heartRate);
+        setData((data) => [
+          ...data,
+          {
+            heartRate: heartRate ? heartRate : undefined,
+            timeStamp: new Date(),
+          },
+        ]);
+      }
+    }, 500);
+    return () => clearInterval(interval);
+  }, [heartRate, isConnected]);
+
   return (
     <ChakraProvider theme={theme}>
       <Center>
@@ -17,6 +41,15 @@ export const App = () => {
             {isConnected ? "Disconnect" : "Connect"}{" "}
           </Button>
           {isConnected ? <Text>HR: {heartRate}</Text> : null}
+          <Stack direction="column-reverse">
+            {data
+              .filter((d) => d.heartRate)
+              .map((d, i) => (
+                <Text key={i}>
+                  {d.timeStamp.toUTCString()} HR: {d.heartRate}
+                </Text>
+              ))}
+          </Stack>
         </Stack>
       </Center>
     </ChakraProvider>
