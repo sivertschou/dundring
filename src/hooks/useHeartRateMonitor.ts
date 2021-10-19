78d2060360ff2269667b99cb133fbafdb3d240b3
Morten Kolstad <morteako@student.matnat.uio.no>
2021-10-19 21:13:49 +0200

    add types to useHeartRateMonitor.ts

--- src/hooks/useHeartRateMonitor.ts
+++ src/hooks/useHeartRateMonitor.ts
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-const parseHeartRate = (value) => {
+const parseHeartRate = (value: DataView | ArrayBuffer) => {
   // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
-  value = value.buffer ? value : new DataView(value);
+  value = value instanceof DataView ? value : new DataView(value);
   let flags = value.getUint8(0);
   let rate16Bits = flags & 0x1;
   if (rate16Bits) {
@@ -14,10 +14,10 @@ const parseHeartRate = (value) => {
 export const useHeartRateMonitor = () => {
   const [heartRate, setHeartRate] = React.useState(0);
   const [isConnected, setIsConnected] = React.useState(false);
-  const [characteristic, setCharacteristic] = React.useState(null);
-  const [device, setDevice] = React.useState(null);
+  const [characteristic, setCharacteristic] = React.useState<BluetoothRemoteGATTCharacteristic | undefined>(undefined);
+  const [device, setDevice] = React.useState<BluetoothDevice | null>(null);
 
-  const handleHRUpdate = (event) => {
+  const handleHRUpdate = (event: any) => {
     const hr = parseHeartRate(event.target.value);
     console.log("handleHRUpdate");
     setHeartRate(hr);
@@ -27,18 +27,18 @@ export const useHeartRateMonitor = () => {
       .requestDevice({ filters: [{ services: ["heart_rate"] }] })
       .then((device) => {
         setDevice(device);
-        return device.gatt.connect();
+        return device?.gatt?.connect();
       })
       .then((server) => {
         setIsConnected(true);
-        return server.getPrimaryService("heart_rate");
+        return server?.getPrimaryService("heart_rate");
       })
       .then((service) => {
-        return service.getCharacteristic("heart_rate_measurement");
+        return service?.getCharacteristic("heart_rate_measurement");
       })
       .then((characteristic) => {
         setCharacteristic(characteristic);
-        return characteristic.startNotifications().then((_) => {
+        return characteristic?.startNotifications().then((_) => {
           console.log("> Notifications started");
           characteristic.addEventListener(
             "characteristicvaluechanged",
@@ -61,8 +61,8 @@ export const useHeartRateMonitor = () => {
             "characteristicvaluechanged",
             handleHRUpdate
           );
-          setCharacteristic(null);
-          device.gatt.disconnect();
+          setCharacteristic(undefined);
+          device?.gatt?.disconnect();
           setDevice(null);
           setIsConnected(false);
         })
