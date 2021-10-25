72d80730cb33306c075709bf8d7609082c0bf23e
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-10-25 20:36:40 +0200

    Fikser #6

--- src/hooks/useSmartTrainer.ts
+++ src/hooks/useSmartTrainer.ts
@@ -30,6 +30,7 @@ export const useSmartTrainer = () => {
     const device = await navigator.bluetooth.requestDevice({
       // filters: [{ services: ["cycling_power"] }], // Cycling power 0x1818
       filters: [{ services: ["fitness_machine"] }], // Fitness machine 0x1826
+      optionalServices: ["cycling_power"],
     });
 
     console.log("CONNECTED AS FITNESS MACHINE");
@@ -56,18 +57,24 @@ export const useSmartTrainer = () => {
     console.log("0x00 sent. res:", res);
 
     console.log("Send 0x01 (Reset)");
-    res = await fitnessMachineCharacteristic?.writeValue(new Uint8Array([0x01]));
+    res = await fitnessMachineCharacteristic?.writeValue(
+      new Uint8Array([0x01])
+    );
     console.log("[0x04, 100] sent. res:", res);
     console.log("fitnessMachineCharacteristic:", fitnessMachineCharacteristic);
 
-    fitnessMachineCharacteristic && setFitnessMachineCharacteristic(fitnessMachineCharacteristic);
+    fitnessMachineCharacteristic &&
+      setFitnessMachineCharacteristic(fitnessMachineCharacteristic);
 
-    const cyclingPowerService = await server?.getPrimaryService("cycling_power");
+    const cyclingPowerService = await server?.getPrimaryService(
+      "cycling_power"
+    );
     console.log("Power service:", cyclingPowerService);
     const cyclingPowerCharacteristic =
       await cyclingPowerService?.getCharacteristic("cycling_power_measurement");
     console.log("powerCharacteristic:", cyclingPowerCharacteristic);
-    cyclingPowerCharacteristic && setCyclingPowerCharacteristic(cyclingPowerCharacteristic);
+    cyclingPowerCharacteristic &&
+      setCyclingPowerCharacteristic(cyclingPowerCharacteristic);
 
     await cyclingPowerCharacteristic?.startNotifications();
     cyclingPowerCharacteristic?.addEventListener(
