e0e0ba935c94065d78332d7f08c8d8cdde715e7f
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-11-03 11:33:33 +0100

    Create required files/folders on install

---
+++ backend/prepare.sh
@@ -0,0 +1,11 @@
+if [ ! -f .env ]; then
+    echo ".env does not exist. Create it"
+    printf "TOKEN_SECRET=\nDATA_PATH=\n" > .env
+fi
+
+if [ ! -d data ]; then
+    echo "data does not exist. Create it"
+    mkdir -p data
+    printf "[]" > data/users.json
+    printf "[]" > data/messages.json
+fi
