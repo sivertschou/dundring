72f801092af331eb6838a155d3f06e911d03af7d
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-12-02 23:20:57 +0100

    Create/join group session on-press-enter

--- frontend/src/components/GroupSession/CreateOrJoinGroupSession.tsx
+++ frontend/src/components/GroupSession/CreateOrJoinGroupSession.tsx
@@ -22,9 +22,10 @@ export const CreateOrJoinGroupSession = () => {
   const [groupSessionId, setGroupSessionId] = React.useState("");
   const [guestUsername, setGuestUsername] = React.useState("");
 
+  const username = user.loggedIn ? user.username : guestUsername;
   const usernameAvailable = user.loggedIn || guestUsername ? true : false;
   const illegalCharacters = removeDuplicateWords(
-    getIllegalUsernameCharacters(guestUsername)
+    getIllegalUsernameCharacters(username)
   );
   const maxUsernameLength = 20;
   const usernameContainsIllegalCharacters = illegalCharacters.length > 0;
@@ -34,86 +35,97 @@ export const CreateOrJoinGroupSession = () => {
 
   return (
     <Stack p="5">
-      {!user.loggedIn ? (
-        <FormControl isInvalid={!usernameIsValid}>
-          <FormLabel>Enter guest username</FormLabel>
-          <Input
-            placeholder="Guest username"
-            type="guest-username"
-            name="guest-username"
-            value={guestUsername}
-            onChange={(e) => {
-              setGuestUsername(e.target.value.replace(" ", ""));
-            }}
-            onBlur={(_e) => {
-              setGuestUsername((guestUsername) => guestUsername.trim());
-            }}
-          />
-          <FormErrorMessage>
-            The username can't
-            {usernameIsTooLong
-              ? ` be more than ${maxUsernameLength} characters long`
-              : ""}
-            {usernameIsTooLong && usernameContainsIllegalCharacters
-              ? " or"
-              : ""}
-            {usernameContainsIllegalCharacters
-              ? ` contain ${illegalCharacters.join(",")}`
-              : ""}
-            .
-          </FormErrorMessage>
-        </FormControl>
-      ) : null}
-
-      <FormControl isInvalid={createStatus === "ERROR"}>
-        <Button
-          onClick={() =>
-            startGroupSession(user.loggedIn ? user.username : guestUsername)
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (usernameAvailable && usernameIsValid) {
+            startGroupSession(username);
           }
-          isDisabled={!usernameAvailable || !usernameIsValid}
-        >
-          Start group session
-        </Button>
-        <FormErrorMessage>Something went wrong.</FormErrorMessage>
-      </FormControl>
+        }}
+      >
+        {!user.loggedIn ? (
+          <FormControl isInvalid={!usernameIsValid}>
+            <FormLabel>Enter guest username</FormLabel>
+            <Input
+              placeholder="Guest username"
+              type="guest-username"
+              name="guest-username"
+              value={guestUsername}
+              onChange={(e) => {
+                setGuestUsername(e.target.value.replace(" ", ""));
+              }}
+              onBlur={(_e) => {
+                setGuestUsername((guestUsername) => guestUsername.trim());
+              }}
+            />
+            <FormErrorMessage>
+              The username can't
+              {usernameIsTooLong
+                ? ` be more than ${maxUsernameLength} characters long`
+                : ""}
+              {usernameIsTooLong && usernameContainsIllegalCharacters
+                ? " or"
+                : ""}
+              {usernameContainsIllegalCharacters
+                ? ` contain ${illegalCharacters.join(",")}`
+                : ""}
+              .
+            </FormErrorMessage>
+          </FormControl>
+        ) : null}
+
+        <FormControl isInvalid={createStatus === "ERROR"}>
+          <Button
+            type="submit"
+            isDisabled={!usernameAvailable || !usernameIsValid}
+          >
+            Start group session
+          </Button>
+          <FormErrorMessage>Something went wrong.</FormErrorMessage>
+        </FormControl>
+      </form>
       <Divider />
 
-      <FormControl>
-        <FormLabel>Join group session</FormLabel>
-        <Input
-          placeholder="Group session id"
-          type="group-session-id"
-          name="group-session-id"
-          value={groupSessionId}
-          onChange={(e) => {
-            setGroupSessionId(e.target.value);
-          }}
-          onBlur={(_e) => {
-            setGroupSessionId((groupSessionId) => groupSessionId.trim());
-          }}
-        />
-      </FormControl>
-      <FormControl
-        isInvalid={!(joinStatus === "NOT_ASKED" || joinStatus === "LOADING")}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (groupSessionId && usernameAvailable && usernameIsValid) {
+            joinGroupSession(groupSessionId, username);
+          }
+        }}
       >
-        <Button
-          onClick={() => {
-            joinGroupSession(
-              groupSessionId,
-              user.loggedIn ? user.username : guestUsername
-            );
-          }}
-          disabled={!groupSessionId || !usernameAvailable}
-          isLoading={joinStatus === "LOADING"}
+        <FormControl>
+          <FormLabel>Join group session</FormLabel>
+          <Input
+            placeholder="Group session id"
+            type="group-session-id"
+            name="group-session-id"
+            value={groupSessionId}
+            onChange={(e) => {
+              setGroupSessionId(e.target.value);
+            }}
+            onBlur={(_e) => {
+              setGroupSessionId((groupSessionId) => groupSessionId.trim());
+            }}
+          />
+        </FormControl>
+        <FormControl
+          isInvalid={!(joinStatus === "NOT_ASKED" || joinStatus === "LOADING")}
         >
-          Join group session
-        </Button>
-        <FormErrorMessage>
-          {joinStatus === "ROOM_NOT_FOUND"
-            ? "Room not found."
-            : "Something went wrong."}
-        </FormErrorMessage>
-      </FormControl>
+          <Button
+            type="submit"
+            disabled={!groupSessionId || !usernameAvailable || !usernameIsValid}
+            isLoading={joinStatus === "LOADING"}
+          >
+            Join group session
+          </Button>
+          <FormErrorMessage>
+            {joinStatus === "ROOM_NOT_FOUND"
+              ? "Room not found."
+              : "Something went wrong."}
+          </FormErrorMessage>
+        </FormControl>
+      </form>
     </Stack>
   );
 };
