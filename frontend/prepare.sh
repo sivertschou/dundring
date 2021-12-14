0999ed2d97b90243c2cfdbbe9b20b76763955d0c
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-12-14 22:32:49 +0100

    Add project formatting & pre-commit formatting

--- frontend/prepare.sh
+++ frontend/prepare.sh
@@ -2,3 +2,8 @@ if [ ! -f .env ]; then
     echo ".env does not exist. Create it"
     printf "REACT_APP_HTTP_URL=http://localhost:8092/api\nREACT_APP_WS_URL=ws://localhost:8092" > .env
 fi
+
+# Make sure parent preparations are done
+echo "Make sure project configs are installed ⚡️"
+cd .. && npm install
+echo "Done configuring project ⚡️"
