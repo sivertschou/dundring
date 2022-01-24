a29be9eadf9117162cf8eceadf09e58dfb205467
Sivert Schou Olsen <sivertschou@hotmail.com>
2022-01-25 00:19:35 +0100

    Make prepare have config on multiple lines

--- frontend/prepare.sh
+++ frontend/prepare.sh
@@ -1,6 +1,14 @@
+config="\
+REACT_APP_DOMAIN=http://localhost:3000
+REACT_APP_HTTP_URL=http://localhost:8080/api
+REACT_APP_WS_URL=ws://localhost:8080
+"
+
 if [ ! -f .env ]; then
-    echo ".env does not exist. Create it"
-    printf "REACT_APP_DOMAIN=http://localhost:3000\nREACT_APP_HTTP_URL=http://localhost:8080/api\nREACT_APP_WS_URL=ws://localhost:8080" > .env
+    echo ".env does not exist. Create it with default values: ⚡️"
+    printf "$config" > .env
+    echo "$config"
+    echo ".env created ⚡️"
 fi
 
 # Make sure parent preparations are done
