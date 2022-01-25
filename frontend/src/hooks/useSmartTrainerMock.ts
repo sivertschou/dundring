a480514b5ac947662a1e4358c2aae2380739d9ff
Sivert Schou Olsen <sivertschou@hotmail.com>
2022-01-25 22:06:08 +0100

    Mock power based on target power

--- frontend/src/hooks/useSmartTrainerMock.ts
+++ frontend/src/hooks/useSmartTrainerMock.ts
@@ -7,20 +7,35 @@ export const useSmartTrainerMock = (): SmartTrainer => {
   const [power, setPower] = React.useState(0);
   const [cadence, setCadence] = React.useState(0);
   const [isConnected, setIsConnected] = React.useState(false);
+  const [targetPower, setTargetPower] = React.useState(0);
+
   const { logEvent } = useLogs();
+
   React.useEffect(() => {
     const interval = setInterval(() => {
       if (isConnected) {
-        setPower((prev) =>
-          randomIntFromIntervalBasedOnPrev(100, 400, prev, 50)
-        );
         setCadence((prev) =>
-          randomIntFromIntervalBasedOnPrev(50, 100, prev, 10)
+          randomIntFromIntervalBasedOnPrev(80, 100, prev, 5)
         );
+
+        if (targetPower) {
+          setPower((prev) =>
+            randomIntFromIntervalBasedOnPrev(
+              targetPower,
+              targetPower + 5,
+              prev,
+              5
+            )
+          );
+        } else {
+          setPower((prev) =>
+            randomIntFromIntervalBasedOnPrev(100, 400, prev, 50)
+          );
+        }
       }
     }, 1000);
     return () => clearInterval(interval);
-  }, [isConnected]);
+  }, [isConnected, targetPower]);
 
   return {
     requestPermission: () => {
@@ -39,6 +54,7 @@ export const useSmartTrainerMock = (): SmartTrainer => {
       (resistance: number) => {
         console.log('MOCK: set resistance: ', resistance);
         logEvent(`[mock] set resistance: ${resistance}W`);
+        setTargetPower(resistance);
       },
       [logEvent]
     ),
