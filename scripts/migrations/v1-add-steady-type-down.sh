089c7ebc95673979db27e8b2f882c5b4a267e167
Morten Kolstad <morten.kolstad@bekk.no>
2022-10-29 14:06:25 +0200

    make scripts use jq (node-jq)

--- scripts/migrations/v1-add-steady-type-down.sh
+++ scripts/migrations/v1-add-steady-type-down.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-jaq -i 'del(.[].workouts[].parts[].type)' apps/backend/data/users.json
+
+echo "creating backup users.backup.json"
+cp apps/backend/data/users.json apps/backend/data/users.backup.down.json
+
+jq 'del(.[].workouts[].parts[].type)' apps/backend/data/users.backup.down.json > apps/backend/data/users.json
+
+echo "Done. Remember to check result and delete backup if OK :))"
+echo "Result:"
+echo $(cat apps/backend/data/users.json)
--- scripts/migrations/v1-add-steady-type-up.sh
+++ scripts/migrations/v1-add-steady-type-up.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-jaq -i '.[].workouts[].parts[] += {type:"steady"}' apps/backend/data/users.json
+
+echo "creating backup users.backup.json"
+cp apps/backend/data/users.json apps/backend/data/users.backup.up.json
+
+jq '.[].workouts[].parts[] += {type:"steady"}' apps/backend/data/users.backup.up.json > apps/backend/data/users.json
+
+echo "Done. Remember to check result and delete backup if OK :)). "
+echo "Result:"
+echo $(cat apps/backend/data/users.json)
