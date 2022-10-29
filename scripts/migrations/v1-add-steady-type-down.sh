463d6bb54bfacdf2905c84c118e5a07b7f025d34
Morten Kolstad <morten.kolstad@bekk.no>
2022-10-29 20:00:44 +0200

    move done-echo last

--- scripts/migrations/v1-add-steady-type-down.sh
+++ scripts/migrations/v1-add-steady-type-down.sh
@@ -5,6 +5,6 @@ cp apps/backend/data/users.json apps/backend/data/users.backup.down.json
 
 npm exec --yes --package=node-jq "jq 'del(.[].workouts[].parts[].type)' apps/backend/data/users.backup.down.json > apps/backend/data/users.json"
 
-echo "Done. Remember to check result and delete backup if OK :))"
 echo "Result:"
 echo $(cat apps/backend/data/users.json)
+echo "Done. Remember to check result and delete backup if OK :))"
--- scripts/migrations/v1-add-steady-type-up.sh
+++ scripts/migrations/v1-add-steady-type-up.sh
@@ -5,6 +5,6 @@ cp apps/backend/data/users.json apps/backend/data/users.backup.up.json
 
 npm exec --yes --package=node-jq "jq '.[].workouts[].parts[] += {type:\"steady\"}' apps/backend/data/users.backup.up.json" > apps/backend/data/users.json
 
-echo "Done. Remember to check result and delete backup if OK :)). "
 echo "Result:"
 echo $(cat apps/backend/data/users.json)
+echo "Done. Remember to check result and delete backup if OK :)). "
