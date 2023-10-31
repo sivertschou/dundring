47273fc28ba0279b65ed07153fc01c9f171c39f2
Petter Sæther Moen <pettersmoen@gmail.com>
2023-10-31 14:19:35 +0100

    improve readability and performance of string utils

--- libs/utils/src/string.ts
+++ libs/utils/src/string.ts
@@ -1,18 +1,17 @@
-export const padLeadingZero = (nr: number) => (nr < 10 ? '0' + nr : nr);
+export const padLeadingZero = (nr: number) => nr.toString().padStart(2, '0');
 
 export const generateRandomString = (length: number) => {
-  const alphabet = '0123456789';
-  const generatedString = new Array(length).fill(0).reduce((result) => {
-    const index = Math.random() * alphabet.length;
-    return result + alphabet.substring(index, index + 1);
-  }, '') as string;
+  const alphabet = '0123456789'.split('');
+  const generatedString = new Array(length)
+    .fill(0)
+    .map(() => {
+      const index = Math.random() * alphabet.length;
+      return alphabet[Math.floor(index)];
+    })
+    .join('');
 
   return generatedString;
 };
 
-export const removeDuplicateWords = (words: string[]) =>
-  words.reduce(
-    (entries, curr) =>
-      entries.every((entry) => entry !== curr) ? [...entries, curr] : entries,
-    [] as string[]
-  );
+export const removeDuplicateWords = (words: string[]): string[] =>
+  Array.from(new Set(words));
