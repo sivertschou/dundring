2c6fc3b0c411c01c560cd7ec4cd3f5c3f72ba837
Sivert Schou Olsen <sivertschou@hotmail.com>
2024-01-13 14:35:34 +0100

    Run frontend at default Vite port 5173

--- apps/frontend/prepare.sh
+++ apps/frontend/prepare.sh
@@ -1,14 +1,14 @@
 config="\
-VITE_DOMAIN=http://localhost:3000
+VITE_DOMAIN=http://localhost:5173
 VITE_HTTP_URL=http://localhost:8080/api
 VITE_WS_URL=ws://localhost:8080
 "
 
 echo "frontend/prepare.sh – Starting ⚡️"
 if [ ! -f .env ]; then
-    echo ".env does not exist. Create it with default values: ⚡️"
-    printf "$config" > .env
-    echo "$config"
-    echo ".env created ⚡️"
+	echo ".env does not exist. Create it with default values: ⚡️"
+	printf "$config" >.env
+	echo "$config"
+	echo ".env created ⚡️"
 fi
 echo "frontend/prepare.sh – Done ⚡️"
