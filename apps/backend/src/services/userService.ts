4f6a8e50b9ff8594404736d6c13b11978a558610
Sivert Schou Olsen <sivertschou@hotmail.com>
2024-02-22 19:18:52 +0100

    Log user update

--- apps/backend/src/services/userService.ts
+++ apps/backend/src/services/userService.ts
@@ -11,7 +11,17 @@ export const getUser = async (query: { username: string } | { id: string }) =>
 export const updateUser = async (
   userId: string,
   data: { username?: string; ftp?: number }
-) => db.updateUser(userId, data);
+) => {
+  const ret = await db.updateUser(userId, data);
+  if (isSuccess(ret)) {
+    slackService.log(
+      `User *${ret.data.id}* updated profile ${
+        data.username ? `username=*${data.username}* ` : ''
+      }${data.ftp ? `ftp=*${data.ftp}*` : ''}`
+    );
+  }
+  return ret;
+};
 
 export const getUserByMail = async (mail: string) => db.getUserByMail(mail);
 export const getUserByStravaId = async (athleteId: number) =>
@@ -29,8 +39,9 @@ export const getUserFitnessData = async (userId: string) =>
 export const createUserFromStrava = async (body: UserCreationStrava) => {
   const ret = await db.createStravaUserWithRandomUsername(body);
   if (isSuccess(ret)) {
+    const athleteId = ret.data.stravaAuthentication?.athleteId;
     slackService.log(
-      `New user: *${ret.data.username}* created with Strava (athlete id: ${ret.data.stravaAuthentication?.athleteId})`
+      `New user: *${ret.data.id}* created with Strava: athlete id: [${athleteId}](https://strava.com/athletes/${ret.data.stravaAuthentication?.athleteId}))`
     );
   }
   return ret;
@@ -40,7 +51,7 @@ export const createUserFromMail = async (mail: string) => {
   const ret = await db.createMailUserWithRandomUsername(mail);
   if (isSuccess(ret)) {
     slackService.log(
-      `New user: *${ret.data.username}* created with mail: ${ret.data.mailAuthentication?.mail}`
+      `New user: *${ret.data.id}* created with mail: ${ret.data.mailAuthentication?.mail}`
     );
   }
   return ret;
