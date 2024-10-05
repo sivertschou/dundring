d04cb9c6f964d0fc28c007b1213677d930544e35
Sivert Schou Olsen <sivertschou@hotmail.com>
2024-10-05 11:43:58 +0200

    feat: Simplify Slack usage

--- apps/backend/src/services/slackService.ts
+++ apps/backend/src/services/slackService.ts
@@ -2,66 +2,15 @@ import fetch from 'node-fetch';
 
 require('dotenv').config();
 
-export const checkSlackConfig = () => {
-  if (!process.env.SLACK_USER_CREATION) {
-    console.log(
-      '[.env]: No Slack service for user creation provided. Override this by setting the SLACK_USER_CREATION in the environment config.'
-    );
-  }
-  if (!process.env.SLACK_ERRORS) {
-    console.log(
-      '[.env]: No Slack service for errors provided. Override this by setting the SLACK_ERRORS in the environment config.'
-    );
-  }
-  if (!process.env.SLACK_GROUP_SESSION) {
-    console.log(
-      '[.env]: No Slack service for group session provided. Override this by setting the SLACK_GROUP_SESSION in the environment config.'
-    );
-  }
-  if (!process.env.SLACK_FEEDBACK) {
-    console.log(
-      '[.env]: No Slack service for feedback provided. Override this by setting the SLACK_FEEDBACK in the environment config.'
-    );
-  }
-  if (!process.env.SLACK_STRAVA_UPLOAD) {
-    console.log(
-      '[.env]: No Slack service for feedback provided. Override this by setting the SLACK_FEEDBACK in the environment config.'
-    );
-  }
-};
-
-enum SlackService {
-  USER_CREATION,
-  ERRORS,
-  GROUP_SESSION,
-  FEEDBACK,
-  STRAVA_UPLOAD,
-}
-
-const getServicePath = (service: SlackService): string | null => {
-  switch (service) {
-    case SlackService.USER_CREATION:
-      return process.env.SLACK_USER_CREATION || null;
-    case SlackService.ERRORS:
-      return process.env.SLACK_ERRORS || null;
-    case SlackService.GROUP_SESSION:
-      return process.env.SLACK_GROUP_SESSION || null;
-    case SlackService.FEEDBACK:
-      return process.env.SLACK_FEEDBACK || null;
-    case SlackService.STRAVA_UPLOAD:
-      return process.env.SLACK_STRAVA_UPLOAD || null;
-  }
-};
-
-const getServiceUrl = (service: SlackService): string | null => {
-  const path = getServicePath(service);
+const getSlackUrl = (): string | null => {
+  const path = process.env.SLACK_TOKEN;
   if (!path) return null;
 
   return `https://hooks.slack.com/services/${path}`;
 };
 
-const sendSlackMessage = (service: SlackService, message: string) => {
-  const url = getServiceUrl(service);
+const sendSlackMessage = (message: string) => {
+  const url = getSlackUrl();
 
   if (process.env.NODE_ENV !== 'production') {
     console.log(`[slack]: ${message}`);
@@ -85,39 +34,39 @@ const sendSlackMessage = (service: SlackService, message: string) => {
 
 export const logActivityUpload = (activityId: number) => {
   const message = `New upload: www.strava.com/activities/${activityId}`;
-  sendSlackMessage(SlackService.STRAVA_UPLOAD, message);
+  sendSlackMessage(message);
 };
 
 export const logUserCreation = (user: { username: string; mail: string }) => {
   const message = `New user: *${user.username}* (${user.mail})`;
-  sendSlackMessage(SlackService.USER_CREATION, message);
+  sendSlackMessage(message);
 };
 
 export const logRoomJoin = (username: string, roomId: string) => {
   const message = `*${username}* joined group session with id *#${roomId}*`;
-  sendSlackMessage(SlackService.GROUP_SESSION, message);
+  sendSlackMessage(message);
 };
 
 export const logRoomCreation = (username: string, roomId: string) => {
   const message = `*${username}* created group session with id *#${roomId}*`;
-  sendSlackMessage(SlackService.GROUP_SESSION, message);
+  sendSlackMessage(message);
 };
 
 export const logRoomLeave = (username: string, roomId: string) => {
   const message = `*${username}* left group session with id *#${roomId}*.`;
-  sendSlackMessage(SlackService.GROUP_SESSION, message);
+  sendSlackMessage(message);
 };
 
 export const logRoomDeletion = (username: string, roomId: string) => {
   const message = `*${username}* left group session with id *#${roomId}*. No more users in group session; deleting it.`;
-  sendSlackMessage(SlackService.GROUP_SESSION, message);
+  sendSlackMessage(message);
 };
 
 export const logAndReturn = <T>(message: string, data: T) => {
-  sendSlackMessage(SlackService.ERRORS, message);
+  sendSlackMessage(message);
   return data;
 };
 
 export const log = (message: string) => {
-  sendSlackMessage(SlackService.ERRORS, message);
+  sendSlackMessage(message);
 };
