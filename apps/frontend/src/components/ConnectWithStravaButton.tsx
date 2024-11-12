ce24136d7aa4c06e59f907acf02471a96ee44ca5
Morten Kolstad <morten.kolstad@bekk.no>
2024-11-12 17:10:17 +0100

    refactor: remove default forcePrompt false param

--- apps/frontend/src/components/ConnectWithStravaButton.tsx
+++ apps/frontend/src/components/ConnectWithStravaButton.tsx
@@ -3,14 +3,14 @@ import { chakra, ImageProps, forwardRef, Link } from '@chakra-ui/react';
 import logo from '../assets/connect-with-strava.svg';
 import { getEnv } from '../utils/environment';
 
-export const getStravaAuthorizeUrl = (params = { forcePrompt: false }) => {
+export const getStravaAuthorizeUrl = (params: { forcePrompt: boolean }) => {
   const clientId = getEnv().stravaClientId;
   return `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${location.origin}/auth/strava&response_type=code&scope=read,activity:write&approval_prompt=${params.forcePrompt ? 'force' : 'auto'}`;
 };
 
 export const ConnectWithStravaButton = () => {
   return (
-    <Link href={getStravaAuthorizeUrl()}>
+    <Link href={getStravaAuthorizeUrl({ forcePrompt: false })}>
       <ConnectWithStravaGraphics />
     </Link>
   );
