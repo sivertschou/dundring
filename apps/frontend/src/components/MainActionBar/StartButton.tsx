32af61d1bc54b7637f9631b2dce53a3681b8df81
Morten Kolstad <morten.kolstad@bekk.no>
2025-01-04 19:27:46 +0100

    feat: Enable space as hotkey for start/stop-button

--- apps/frontend/src/components/MainActionBar/StartButton.tsx
+++ apps/frontend/src/components/MainActionBar/StartButton.tsx
@@ -1,12 +1,34 @@
 import { Button, Icon } from '@chakra-ui/react';
 import { PauseFill, PlayFill } from 'react-bootstrap-icons';
 import { useData } from '../../context/DataContext';
+import * as React from 'react';
 
 export const StartButton = () => {
   const { hasValidData, isRunning, start, stop } = useData();
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
+
+  React.useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // Ignore keypress for pages that is not the start-page, like /workout and /group
+      if (window.location.pathname !== '/') {
+        return;
+      }
+      if (event.code === 'Space') {
+        event.preventDefault();
+        buttonRef.current?.click();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   if (isRunning)
     return (
       <Button
+        ref={buttonRef}
         width="100%"
         size="lg"
         onClick={stop}
@@ -18,6 +40,7 @@ export const StartButton = () => {
 
   return (
     <Button
+      ref={buttonRef}
       width="100%"
       size="lg"
       onClick={start}
