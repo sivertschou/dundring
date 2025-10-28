79210e5ef27f0fb296df0edce9f7cb2501fadb89
Morten Kolstad <morten.kolstad@bekk.no>
2025-10-28 19:40:13 +0100

    Revert "fix: Base smoothed power on all data"
    This reverts commit 56335f8d8becbade096d6c97395c09d211c94ef2.

--- apps/frontend/src/context/DataContext.tsx
+++ apps/frontend/src/context/DataContext.tsx
@@ -161,7 +161,7 @@ export const DataContextProvider = ({ clockWorker, children }: Props) => {
         },
         state,
         isRunning,
-        smoothedPower: getSmoothedPower(graphData),
+        smoothedPower: getSmoothedPower(trackedData),
         maxHeartRate: workoutState.maxHeartRate || null,
       }}
     >
